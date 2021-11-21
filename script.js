d2a2e24f519d66caff8ce05fdc798b56ed035d33
Gabriel Medeiros <g4brielmedeiros@pm.me>
2021-11-21 02:19:09 -0300

    added tests and started script

---
+++ script.js
@@ -0,0 +1,31 @@
+const roundToNearestThousandth = (number) => Math.round(number * 10000) / 10000;
+
+function operate(operator, x, y) {
+  let result = 0;
+
+  switch (operator) {
+    case "+":
+      result = x + y;
+      break;
+
+    case "-":
+      result = x - y;
+      break;
+
+    case "*":
+      result = x * y;
+      break;
+
+    case "/":
+      result = roundToNearestThousandth(x / y);
+      break;
+
+    default:
+      result = "error";
+      break;
+  }
+
+  return result == Infinity ? "bruh" : result;
+}
+
+module.exports = operate;
---
+++ tests/script.test.js
@@ -0,0 +1,33 @@
+const operate = require("../script.js");
+
+describe("operate function", () => {
+ 
+  it("adds", () => {
+    expect(operate("+", 3, 2)).toBe(5);
+    expect(operate("+", 0, 2)).toBe(2);
+    expect(operate("+", 3, -2)).toBe(1);
+  });
+
+
+
+  it("subtracts", () => {
+    expect(operate("-", 3, 2)).toBe(1);
+    expect(operate("-", 2, 3)).toBe(-1);
+    expect(operate("-", 0, 2)).toBe(-2);
+    expect(operate("-", 4, 0)).toBe(4);
+  });
+
+  it("multiplies", () => {
+    expect(operate("*", 3, 2)).toBe(6);
+    expect(operate("*", 0, 2)).toBe(0);
+    expect(operate("*", 2, 0)).toBe(0);
+    expect(operate("*", 1.2, 2.8)).toBe(3.36);
+  });
+
+  it("divides", () => {
+    expect(operate("/", 6, 2)).toBe(3);
+    expect(operate("/", 4, 3)).toBe(1.3333);
+    expect(operate("/", 1, 0)).toBe('bruh');
+    expect(operate("/", 6, 1)).toBe(6);
+  });
+})
