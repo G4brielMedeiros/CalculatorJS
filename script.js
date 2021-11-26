b62d40566e6fcb85e542803b7d4b6ec36d63c883
Gabriel Medeiros <g4brielmedeiros@pm.me>
2021-11-25 23:18:22 -0300

    feature added: typing on the display

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-const roundToNearestThousandth = (number) => Math.round(number * 10000) / 10000;
+const digitsDOM = document.querySelectorAll(".digit");
+const displayDOM = document.getElementById("expression");
+const dotDOM = document.getElementById("dot");
+const acDOM = document.getElementById("ac");
+
+const displayLimit = 20;
 
 function operate(operator, x, y) {
   let result = 0;
@@ -17,15 +22,43 @@ function operate(operator, x, y) {
       break;
 
     case "/":
-      result = (x / y);
+      result = x / y;
       break;
 
     default:
-      result = "error";
+      result = x;
       break;
   }
 
-  return result == Infinity ? "bruh" : roundToNearestThousandth(result);
+  return result == Infinity ? "bruh" : Math.round(result * 10000) / 10000;
 }
 
+
+function displayDigit(digitDOM) {
+  return () => {
+    const DIGIT = digitDOM.textContent;
+    const TEXT = displayDOM.textContent;
+
+
+    if ( (TEXT.length < displayLimit) && (TEXT.substr(TEXT.indexOf(".")).length != 17) ) {
+
+      if (TEXT.includes(".") && DIGIT.includes(".")) return;
+
+      displayDOM.textContent += DIGIT;
+    }
+
+  };
+}
+
+
+Array.from(digitsDOM).map((digitDOM) => {
+  digitDOM.addEventListener("click", displayDigit(digitDOM));
+});
+
+acDOM.addEventListener("click", () => displayDOM.textContent = "");
+
 module.exports = operate;
+
+console.log(1/3);
+
+
